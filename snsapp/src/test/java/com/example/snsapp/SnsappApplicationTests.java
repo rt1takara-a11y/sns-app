4516cea4abaeb06ae04edcbab6ba2b988b42ee07
rt1takara-a11y <rt1.takara@gmail.com>
2025-09-01 00:02:45 +0900

    Add initial Spring Boot project

---
+++ snsapp/src/test/java/com/example/snsapp/SnsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
